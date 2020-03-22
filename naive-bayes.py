9c91deefaf944955d806cdd1e611371f7e81b0a5
Joseph Kahara <joekahara@gmail.com>
2020-03-22 01:42:12 -0400

    Finished prediction and metrics

--- naive-bayes.py
+++ naive-bayes.py
@@ -3,7 +3,10 @@ import numpy as np
 
 
 def naive_bayes(data):
+    # Set m for m-estimate naive-bayes arbitrarily
+    m = 30
 
+    # ---------------- Cross Validation Prep ---------------- #
     # Split DataFrame by class to prepare for stratified cross validation
     negative_df, positive_df = [x for _, x in data.groupby(data.Class == 'positive')]
     # Split data for each class into 5 folds
@@ -24,41 +27,93 @@ def naive_bayes(data):
         working_data = folds.copy()
         # Create training and testing sets
         test = working_data.pop(i)
-        test = test.drop('Class', axis=1)
-        # print(test.shape)
         train = pd.concat(working_data)
-        # print(train.shape)
 
-        # Find class probabilities
-        class_counts = train.Class.value_counts()
-        # Create dict to track class values and their count
-        class_counts = dict(zip(class_counts.index, class_counts))
-        total_count = sum(class_counts.values())
+        # ---------------- Probability Calculations ---------------- #
+        # Find distinct classes
+        class_values = train.Class.unique()
         # Calculation of class priors
-        prior_negative = class_counts['negative'] / total_count
-        prior_positive = class_counts['positive'] / total_count
-        # print(prior_positive, " ", prior_negative, " ", (prior_negative + prior_positive))
+        prior_probabilities = train.Class.value_counts() / train.shape[0]
 
+        conditional_probabilities = {}
         # Iterate through features to calculate probabilities with all their values
         for feature, values in train.iloc[:, :-1].iteritems():
-            value_counts = values.value_counts()
-            # Create dict to track feature values and their counts
-            value_counts = dict(zip(value_counts.index, value_counts))
+            feature_values = values.unique()
+            # Initialize numpy array with 0s to store feature/class value conditional probability
+            feature_conditional_probability = dict.fromkeys(class_values, dict.fromkeys(feature_values))
+            for j in range(len(class_values)):
+                for k in range(len(feature_values)):
+                    # Calculate conditional probability for each feature value with both classes
+                    # P(x|c) = (n_c + mp) / (n + m)
+                    # x = feature value, c = class, n_c = # of feature values for given class,
+                    # p = class prior, n = # of class values for given class
+                    feature_conditional_probability[class_values[j]][feature_values[k]] = \
+                        ((train.loc[(values == feature_values[k]) &
+                                    (data.Class == class_values[j]), ].shape[0]) / \
+                        (sum(train.Class == class_values[j])))
+            conditional_probabilities[feature] = feature_conditional_probability
 
-            print(value_counts)
+        # ---------------- Prediction ---------------- #
+        predictions = []
+        # Iterate through test set rows to predict each one's class
+        for row in test.itertuples(index=False):
+            # Initialize product of feature values' conditional probabilities
+            negative_conditionals, positive_conditionals = 1, 1
+            for j in range(test.iloc[:, :-1].shape[1]):
+                # For each feature, continue to calculate the product
+                negative_conditionals *= conditional_probabilities[test.columns[j]][class_values[0]][row[j]]
+                positive_conditionals *= conditional_probabilities[test.columns[j]][class_values[1]][row[j]]
+            # Calculate prediction probabilities
+            prob_of_negative = prior_probabilities[0] * negative_conditionals
+            prob_of_positive = prior_probabilities[1] * positive_conditionals
+            # Predict class according to calculated probabilities
+            if prob_of_negative > prob_of_positive:
+                predictions.append(class_values[0])
+            else:
+                predictions.append(class_values[1])
+        # Add predictions to the test dataset then print
+        test = test.assign(Prediction=predictions)
+        print(test)
+
+        # ---------------- Performance Metrics ---------------- #
+        # Count and print confusion matrix
+        tp, tn, fp, fn = 0, 0, 0, 0
+        for j in range(test.shape[0]):
+            if test.iloc[j, -1] == 'negative':
+                if test.iloc[j, -2] == 'negative':
+                    tn += 1
+                else:
+                    fn += 1
+            else:
+                if test.iloc[j, -2] == 'positive':
+                    tp += 1
+                else:
+                    fp += 1
+        print("TP: ", tp, " TN: ", tn, " FP: ", fp, " FN: ", fn)
+        # Calculate and print performance metrics
+        accuracy = (tp + tn) / test.shape[0]
+        if tp == 0 or fp == 0:
+            sensitivity = 'NaN'
+        else:
+            sensitivity = tp / (tp + fp)
+        if tn == 0 or fp == 0:
+            specificity = 'NaN'
+        else:
+            specificity = tn / (tn + fp)
+        print("Accuracy = ", accuracy, ", sensitivity = ", sensitivity, ", specificity = ", specificity)
+        # Combine test and training sets to recreate original dataset with added predictions
+        pd.concat(list([test, train])).to_csv('predictions.csv')
+
 
 # Read phone marketing data from CSV file
 dataset = pd.read_csv("naive-bayes-data.csv")
 
 # Encode numeric age to a categorical feature (grouping not perfectly accurate, estimated)
 categorical_ages = pd.cut(dataset.Age,
-                          bins=[17, 25, 35, 40, 50, 65, 99],
-                          labels=['17-25', '25-35', '35-40', '40-50', '50-65', '65-99'])
+                          bins=[16, 25, 35, 40, 50, 65, 99],
+                          labels=['16-25', '25-35', '35-40', '40-50', '50-65', '65-99'])
 dataset.insert(0, "Age_Group", categorical_ages)
 dataset.drop(columns=['Age'], inplace=True)
-
-# print("Distribution among age categories:")
-# print(df.Age_Group.value_counts().sort_index(), "\n")
 
 # Rename class column to 'class'
 dataset.rename(columns={"Subscribed": "Class"}, inplace=True)
